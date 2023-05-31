636a2c7a8f563b843ef4cf4beba16ced3914be86
olami237 <127667864+olami237@users.noreply.github.com>
2023-05-31 14:29:19 +0100

    Add files via upload

---
+++ sailproject/assesment/index.js
@@ -0,0 +1,14 @@
+let big = document.getElementById('img')
+let fullName = document.getElementById("/fullname")
+let title = document.getElementById("/title")
+let street = document.getElementById("street")
+let phoneNumber = document.getElementById(phoneNumber)
+const url = "http://randomuser.me/api/";
+
+let data = () => {
+
+
+
+
+
+    
